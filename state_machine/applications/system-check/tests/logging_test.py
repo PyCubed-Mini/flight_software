4a910455a62580fd7c64ad37341faf7dc4c3e36c
yashikabatra15 <56561968+yashikabatra15@users.noreply.github.com>
2022-07-20 00:59:13 -0700

    implement simple logging test

--- state_machine/applications/system-check/tests/logging_test.py
+++ state_machine/applications/system-check/tests/logging_test.py
@@ -5,13 +5,55 @@ SD Card Logging Test
 * Author(s): Yashika Batra
 """
 
-# copy and paste logging functions and use them
+from os import exists, remove
 
-def run(cubesat, hardware_dict, result_dict):
+def sd_test():
+    """
+    Run a basic test; create a file and test existence, write to and read from a file,
+    delete file and test existence. Return result_dict values accordingly
+    """
+
+    # create file
+    test_fp = "/sd/test.txt"
+    test_file = open(test_fp)
+
+    # check that the file exists:
+    if not exists(test_fp):
+        return ("Created a file but system says it does not exist.", False)
+
+    # write to file
+    test_string = "Hello World! This is a test file.\n"
+    test_file.write(test_string)
+    test_file.close()
+
+    # read from file
+    test_file_read = open(test_fp)
+    test_string_read = test_file_read.read()
+    if test_string_read != test_string:
+        return ("File not written to or read from correctly.", False)
+
+    # delete file
+    remove(test_file)
+
+    # check that the file no longer exists:
+    if exists(test_fp):
+        return ("File not successfully deleted.", False)
+
+    return ("SD Card passed all tests: Created, wrote to, " +
+            "read from, and deleted a file successfully.", True)
+
+
+def run(hardware_dict, result_dict):
+    """
+    Check that the correct hardware is initialized and run tests
+    """
+
     # if no SD Card detected, update result dictionary and return
     if not hardware_dict['SDcard']:
-        result_dict['SD_Card_Logging'] = ('Cannot test logging; no SD Card detected', False)
+        result_dict['SDcard_Logging'] = ('Cannot test logging; no SD Card detected', False)
         return result_dict
 
     # if SD Card detected, run other tests
+    result_val_string, result_val_bool = sd_test()
+    result_dict['SDcard_Logging'] = (result_val_string, result_val_bool)
     return result_dict
