89402f4e844b6e9ccacde8dd07c01061d8fd2b11
yashika-batra <56561968+yashika-batra@users.noreply.github.com>
2022-07-26 01:50:13 -0700

    update to support circuitpython os module + style

--- state_machine/applications/system-check/tests/logging_test.py
+++ state_machine/applications/system-check/tests/logging_test.py
@@ -5,40 +5,60 @@ SD Card Logging Test
 * Author(s): Yashika Batra
 """
 
-from os import exists, remove
+import os
+
 
 def sd_test():
     """
-    Run a basic test; create a file and test existence, write to and read from a file,
-    delete file and test existence. Return result_dict values accordingly
+    Run a basic test; create a file and test existence, write to and
+    read from a file, delete file and test existence. Return result_dict
+    values accordingly
     """
 
-    # create file
-    test_fp = "/sd/test.txt"
-    test_file = open(test_fp)
+    # create filepaths
+    fp = "test.txt"
+    test_fp = "/sd/" + fp
 
-    # check that the file exists:
-    if not exists(test_fp):
-        return ("Created a file but system says it does not exist.", False)
+    # try to create a file with the test filepath
+    try:
+        test_file = open(test_fp, "x")
+    except OSError:
+        # if the file already exists, remove it and create a new file
+        os.remove(test_fp)
+        test_file = open(test_fp, "x")
+
+    print("Directory after test.txt was created: ", os.listdir("/sd/"))
+    if fp not in os.listdir("/sd/"):
+        return ("File creation failed. ", False)
 
     # write to file
     test_string = "Hello World! This is a test file.\n"
-    test_file.write(test_string)
+    try:
+        test_file.write(test_string)
+    except OSError as e:
+        print("Unable to write to file. " + str(e))
+        return ("Unable to write to file. " + str(e), False)
     test_file.close()
 
     # read from file
-    test_file_read = open(test_fp)
-    test_string_read = test_file_read.read()
+    try:
+        test_file_read = open(test_fp, "r")
+        test_string_read = test_file_read.read()
+    except OSError as e:
+        print("Unable to read from file. " + str(e))
+        return ("Unable to read from file. " + str(e), False)
+
     if test_string_read != test_string:
+        print("File not written to or read from correctly.")
         return ("File not written to or read from correctly.", False)
 
     # delete file
-    remove(test_file)
+    os.remove(test_fp)
+    print("Directory after test.txt was removed: ", os.listdir("/sd/"))
+    if fp in os.listdir("/sd/"):
+        return ("File deletion failed. ", False)
 
-    # check that the file no longer exists:
-    if exists(test_fp):
-        return ("File not successfully deleted.", False)
-
+    # if nothing has failed so far, return success
     return ("SD Card passed all tests: Created, wrote to, " +
             "read from, and deleted a file successfully.", True)
 
@@ -50,7 +70,8 @@ def run(hardware_dict, result_dict):
 
     # if no SD Card detected, update result dictionary and return
     if not hardware_dict['SDcard']:
-        result_dict['SDcard_Logging'] = ('Cannot test logging; no SD Card detected', False)
+        result_dict['SDcard_Logging'] = (
+            'Cannot test logging; no SD Card detected', False)
         return result_dict
 
     # if SD Card detected, run other tests
