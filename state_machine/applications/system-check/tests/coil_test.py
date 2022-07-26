5156e2fb3ecf36a29ce48e9fbd50aedb7f34cb11
yashika-batra <56561968+yashika-batra@users.noreply.github.com>
2022-07-26 01:45:58 -0700

    update to reflect changes in driver, style fixes

--- state_machine/applications/system-check/tests/coil_test.py
+++ state_machine/applications/system-check/tests/coil_test.py
@@ -9,11 +9,16 @@ import time
 from lib import pycubed as cubesat
 from math import sqrt
 
-# voltage level constants; set between 0 and 1
-# get these values from the hex values in doc
-v1 = 0x0A
-v2 = 0x15
-v3 = 0x1F
+# voltage level constants; set between -1 and 1
+v1 = 0.25
+v2 = 0.5
+v3 = 0.75
+
+# find projected voltage: vlevel * max voltage
+projected_voltage1 = v1 * 5.06
+projected_voltage2 = v2 * 5.06
+projected_voltage3 = v3 * 5.06
+
 
 def user_test(coil_index):
     """
@@ -23,46 +28,43 @@ def user_test(coil_index):
     """
 
     # wait times
-    wait_time = 30
-    driver_time = 10
-
-    # formula in drv8830 docs:
-    # https://www.ti.com/lit/ds/symlink/drv8830.pdf?ts=1657017752384&ref_url=https%253A%252F%252Fwww.ti.com%252Fproduct%252FDRV8830
-    projected_voltage1 = 4 * 1.285 * int(v1) / 64
-    projected_voltage2 = 4 * 1.285 * int(v2) / 64
-    projected_voltage3 = 4 * 1.285 * int(v3) / 64
+    wait_time = 3
+    driver_time = 3
 
     # set up the test
-    print("Testing Coil", str(coil_index), "for the following voltage levels: " +
-          str(projected_voltage1) + ", " + str(projected_voltage2 + ", " +
-          str(projected_voltage3)))
+    print("Testing Coil", str(coil_index),
+          "for the following voltage levels: " + str(projected_voltage1) +
+          ", " + str(projected_voltage2) + ", " + str(projected_voltage3))
     print("Waiting", str(wait_time), "seconds.")
     time.sleep(wait_time)
 
-    # figure out what pins we need to be reading
     # conduct the test
-    print("Beginning the test now. Testing for", str(driver_time * 3), "seconds.")
+    print("Beginning the test now. Testing for", str(driver_time * 3),
+          "seconds.")
 
     # test each voltage level
-    cubesat.coildriver_vout(coil_index, v1)
+    cubesat.coildriver_vout(coil_index, projected_voltage1)
     time.sleep(driver_time)
     mag_reading1 = cubesat.magnetic()
 
-    cubesat.coildriver_vout(coil_index, v2)
+    cubesat.coildriver_vout(coil_index, projected_voltage2)
     time.sleep(driver_time)
     mag_reading2 = cubesat.magnetic()
 
-    cubesat.coildriver_vout(coil_index, v3)
+    cubesat.coildriver_vout(coil_index, projected_voltage3)
     time.sleep(driver_time)
     mag_reading3 = cubesat.magnetic()
 
     # calculate total magnetic reading
     print("Data Collection Complete")
-    mag_total1 = sqrt(mag_reading1[0] ** 2 + mag_reading1[1] ** 2 + mag_reading1[2] ** 2)
-    mag_total2 = sqrt(mag_reading2[0] ** 2 + mag_reading2[1] ** 2 + mag_reading2[2] ** 2)
-    mag_total3 = sqrt(mag_reading3[0] ** 2 + mag_reading3[1] ** 2 + mag_reading3[2] ** 2)
+    mag_total1 = sqrt(
+        mag_reading1[0] ** 2 + mag_reading1[1] ** 2 + mag_reading1[2] ** 2)
+    mag_total2 = sqrt(
+        mag_reading2[0] ** 2 + mag_reading2[1] ** 2 + mag_reading2[2] ** 2)
+    mag_total3 = sqrt(
+        mag_reading3[0] ** 2 + mag_reading3[1] ** 2 + mag_reading3[2] ** 2)
 
-    # if magnetometer readings are increasing over time, return true. else, false
+    # if magnetometer readings are increasing over time, return true
     return (mag_total3 > mag_total2 and mag_total2 > mag_total1)
 
 
@@ -77,12 +79,16 @@ def voltage_levelx(result_dict, coil_index):
 
     # get user test result values, process and print results
     result = user_test(coil_index)
-    result_val_string = ('Tested Coil Driver ' + str(coil_index) + ' at the following voltage levels: ' +
-                         str(v1) + ', ' + str(v2) + ', ' + str(v3) + '.')
+    result_val_string = ('Tested Coil Driver ' + str(coil_index) +
+                         ' at the following voltage levels: ' +
+                         str(projected_voltage1) + ', ' +
+                         str(projected_voltage2) + ', ' +
+                         str(projected_voltage3) + '.')
     if result:
-        result_val_string += 'Magnetometer readings changed as expected with voltage levels.'
+        result_val_string += 'Magnetometer readings changed as expected.'
     else:
-        result_val_string += 'Magnetometer readings did not change as expected.'
+        result_val_string += ('Magnetometer readings did not change as' +
+                              'expected.')
 
     # update result dictionary
     result_dict[result_key] = (result_val_string, result)
