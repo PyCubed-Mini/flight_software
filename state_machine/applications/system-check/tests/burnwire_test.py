1bc7d4e779b46d92bc2a8b29c68abf5b86f78a92
yashika-batra <56561968+yashika-batra@users.noreply.github.com>
2022-11-18 16:05:06 -0500

    standardize headers, remove authors

--- state_machine/applications/system-check/tests/burnwire_test.py
+++ state_machine/applications/system-check/tests/burnwire_test.py
@@ -1,8 +1,6 @@
 """
-Python system check script for PyCubed satellite board
-PyCubed Mini mainboard-v02 for Pocketqube Mission
+Python System Check Module for PyCubed Mini satellite board
 Burnwire Deployment Test
-* Author(s): Yashika Batra
 """
 
 from lib.pycubed import cubesat
