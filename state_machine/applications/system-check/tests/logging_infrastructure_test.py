b6ecf69c7177c913bcc67c538f80afeb3dc2bf98
yashika-batra <56561968+yashika-batra@users.noreply.github.com>
2022-11-18 16:07:35 -0500

    standardize headers/remove authors

--- state_machine/applications/system-check/tests/logging_infrastructure_test.py
+++ state_machine/applications/system-check/tests/logging_infrastructure_test.py
@@ -1,3 +1,8 @@
+"""
+Python System Check Module for PyCubed Mini satellite board
+Logging Infrastructure Test
+"""
+
 from logging import clear_all_storage, get_buffer, log
 from os import listdir
 from lib.pycubed import cubesat
