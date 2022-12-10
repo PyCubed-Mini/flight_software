525cd9941280633609926615a0f99908debd8288
Jacob Willis <jbwillis272@gmail.com>
2022-12-09 20:32:58 -0500

    use tasko, integrate with commands.py

--- groundstation/gs_actions.py
+++ groundstation/gs_actions.py
@@ -2,33 +2,89 @@
 Provides individual groundstation actions such as upload a file,
 wait for packet, or send a command.
 """
+import board
+import busio
+import digitalio
+from lib import pycubed_rfm9x_fsk
 from radio_utils.disk_buffered_message import DiskBufferedMessage
 from radio_utils import headers
 from radio_utils.commands import super_secret_code
+from lib.configuration import radio_configuration as rf_config
 
-def initialize_radio():
+def initialize_radio(cs, reset):
+    """
+    Initialize the radio - uses lib/configuration/radio_configuration to configure with defaults
+    """
+
+    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+    radio = pycubed_rfm9x_fsk.RFM9x(
+        spi,
+        cs,
+        reset,
+        rf_config.FREQUENCY,
+        checksum=rf_config.CHECKSUM)
+
+    # configure to match satellite
+    radio.tx_power = rf_config.TX_POWER
+    radio.bitrate = rf_config.BITRATE
+    radio.frequency_deviation = rf_config.FREQUENCY_DEVIATION
+    radio.rx_bandwidth = rf_config.RX_BANDWIDTH
+    radio.preamble_length = rf_config.PREAMBLE_LENGTH
+    radio.ack_delay = rf_config.ACK_DELAY
+    radio.ack_wait = rf_config.ACK_WAIT
+    radio.node = rf_config.GROUNDSTATION_ID
+    radio.destination = rf_config.SATELLITE_ID
+
+    return radio
+
+def satellite_cs_reset():
+    # pocketqube
+    cs = digitalio.DigitalInOut(board.RF_CS)
+    reset = digitalio.DigitalInOut(board.RF_RST)
+    cs.switch_to_output(value=True)
+    reset.switch_to_output(value=True)
+
+    radio_DIO0 = digitalio.DigitalInOut(board.RF_IO0)
+    radio_DIO0.switch_to_input()
+    radio_DIO1 = digitalio.DigitalInOut(board.RF_IO1)
+    radio_DIO1.switch_to_input()
+
+    return cs, reset
+
+def feather_cs_reset():
+    # feather
+    cs = digitalio.DigitalInOut(board.D5)
+    reset = digitalio.DigitalInOut(board.D6)
+    cs.switch_to_output(value=True)
+    reset.switch_to_output(value=True)
+
+    return cs, reset
+
+def pi_cs_reset():
+    cs = digitalio.DigitalInOut(board.CE1)
+    reset = digitalio.DigitalInOut(board.D25)
+
+    return cs, reset
+
+
+async def wait_for_message(radio):
     pass
 
-def setup_board():
-    pass
-
-def wait_for_message(radio):
-    pass
-
-def send_command(radio, command_bytes, args, will_respond):
+async def send_command(radio, command_bytes, args, will_respond):
     msg = bytes([headers.COMMAND]) + super_secret_code + command_bytes + bytes(args, 'utf-8')
-    if radio.send_with_ack(msg, debug=True):
+    if await radio.send_with_ack(msg, debug=True):
         print('Successfully sent command')
         if will_respond:
             print('Waiting for response')
-            wait_for_message(radio)
+            await wait_for_message(radio)
     else:
         print('Failed to send command')
 
 def download_file():
     pass
 
-def upload_file(radio, path):
+async def upload_file(radio, path):
     msg = DiskBufferedMessage(0, path)
     while True:
         packet, with_ack = msg.packet()
@@ -37,14 +93,14 @@ def upload_file(radio, path):
         print(f"Sending packet: {debug_packet}, with_ack: {with_ack}")
 
         if with_ack:
-            if radio.send_with_ack(packet, debug=True):
+            if await radio.send_with_ack(packet, debug=True):
                 print('ack')
                 msg.ack()
             else:
                 print('no ack')
                 msg.no_ack()
         else:
-            radio.send(packet, keep_listening=True)
+            await radio.send(packet, keep_listening=True)
 
         if msg.done():
             break
--- groundstation/gs_shell.py
+++ groundstation/gs_shell.py
@@ -1,112 +1,81 @@
 """
 Provides a basic shell-like interface to send and receive data from the satellite
 """
-import board
-import busio
-import digitalio
-from lib import pycubed_rfm9x_fsk
-from lib import radio_defaults
-from radio_utils.commands import command_map
+from radio_utils.commands import commands
 from shell_utils import bold, normal, red, green, yellow, blue, get_input_discrete, manually_configure_radio, print_radio_configuration
-from gs_actions import upload_file, send_command, wait_for_message
+from gs_actions import *
+import tasko
+
+commands_by_name = {
+    commands[cb]["name"]:
+    {"bytes": cb, "will_respond": commands[cb]["will_respond"], "has_args": commands[cb]["has_args"]}
+    for cb in commands.keys()}
 
 
-print(f"\n{bold}{yellow}PyCubed-Mini Groundstation Shell{normal}\n")
+async def gs_shell_main():
+    print(f"\n{bold}{yellow}PyCubed-Mini Groundstation Shell{normal}\n")
 
-board_str = get_input_discrete(
-    f"Select the board {bold}(s){normal}atellite, {bold}(f){normal}eather, {bold}(r){normal}aspberry pi",
-    ["s", "f", "r"]
-)
+    board_str = get_input_discrete(
+        f"Select the board {bold}(s){normal}atellite, {bold}(f){normal}eather, {bold}(r){normal}aspberry pi",
+        ["s", "f", "r"]
+    )
 
-if board_str == "s":
-    # pocketqube
-    CS = digitalio.DigitalInOut(board.RF_CS)
-    RESET = digitalio.DigitalInOut(board.RF_RST)
-    CS.switch_to_output(value=True)
-    RESET.switch_to_output(value=True)
+    if board_str == "s":
+        cs, reset = satellite_cs_reset()
+        print(f"{bold}{green}Satellite{normal} selected")
+    elif board_str == "f":
+        cs, reset = feather_cs_reset()
+        print(f"{bold}{green}Feather{normal} selected")
+    else:  # board_str == "r"
+        cs, reset = pi_cs_reset()
+        print(f"{bold}{green}Raspberry Pi{normal} selected")
 
-    radio_DIO0 = digitalio.DigitalInOut(board.RF_IO0)
-    radio_DIO0.switch_to_input()
-    radio_DIO1 = digitalio.DigitalInOut(board.RF_IO1)
-    radio_DIO1.switch_to_input()
+    radio = initialize_radio(cs, reset)
 
-    print(f"{bold}{green}Satellite{normal} selected")
-elif board_str == "f":
-    # feather
-    CS = digitalio.DigitalInOut(board.D5)
-    RESET = digitalio.DigitalInOut(board.D6)
-    CS.switch_to_output(value=True)
-    RESET.switch_to_output(value=True)
-
-    print(f"{bold}{green}Feather{normal} selected")
-else:  # board_str == "r"
-    # raspberry pi
-    CS = digitalio.DigitalInOut(board.CE1)
-    RESET = digitalio.DigitalInOut(board.D25)
-    print(f"{bold}{green}Raspberry Pi{normal} selected")
-
-# Initialize SPI bus.
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-
-# Initialze radio
-radio = pycubed_rfm9x_fsk.RFM9x(
-    spi,
-    CS,
-    RESET,
-    radio_defaults.FREQUENCY,
-    checksum=radio_defaults.CHECKSUM)
-
-# configure to match satellite
-radio.tx_power = radio_defaults.TX_POWER
-radio.bitrate = radio_defaults.BITRATE
-radio.frequency_deviation = radio_defaults.FREQUENCY_DEVIATION
-radio.rx_bandwidth = radio_defaults.RX_BANDWIDTH
-radio.preamble_length = radio_defaults.PREAMBLE_LENGTH
-radio.ack_delay = radio_defaults.ACK_DELAY
-radio.ack_wait = radio_defaults.ACK_WAIT
-radio.node = radio_defaults.GROUNDSTATION_ID
-radio.destination = radio_defaults.SATELLITE_ID
-
-print_radio_configuration(radio)
-
-if "y" == get_input_discrete(
-        f"Change radio parameters? {bold}(y/N){normal}", ["", "y", "n"]):
-    manually_configure_radio(radio)
     print_radio_configuration(radio)
 
-prompt_options = {"Receive loop": ("r", "receive"),
-                  "Upload file": ("u", "upload"),
-                  "Send command": ("c", "command"),
-                  "Help": ("h", "print_help"),
-                  "Quit": ("q", "quit")}
+    if "y" == get_input_discrete(
+            f"Change radio parameters? {bold}(y/N){normal}", ["", "y", "n"]):
+        manually_configure_radio(radio)
+        print_radio_configuration(radio)
 
-def print_help():
-    print(f"\n{yellow}Groundstation shell help:{normal}")
-    for po in prompt_options:
-        print(f"{bold}{po}{normal}: {prompt_options[po]}")
+    prompt_options = {"Receive loop": ("r", "receive"),
+                      "Upload file": ("u", "upload"),
+                      "Send command": ("c", "command"),
+                      "Help": ("h", "print_help"),
+                      "Quit": ("q", "quit")}
+
+    def print_help():
+        print(f"\n{yellow}Groundstation shell help:{normal}")
+        for po in prompt_options:
+            print(f"{bold}{po}{normal}: {prompt_options[po]}")
+
+    print_help()
+
+    while True:
+        flattend_prompt_options = [v for pov in prompt_options.values() for v in pov]
+        choice = get_input_discrete("Choose an action", flattend_prompt_options)
+        if choice in prompt_options["Receive loop"]:
+            print("Entering receive loop. CTRL-C to exit")
+            while True:
+                try:
+                    print(await wait_for_message(radio))
+                except KeyboardInterrupt:
+                    break
+        elif choice in prompt_options["Upload file"]:
+            path = input('path= ')
+            await upload_file(radio, path)
+        elif choice in prompt_options["Send command"]:
+            command_name = get_input_discrete("Select a command", list(commands_by_name.keys())).upper()
+            command_bytes = commands_by_name[command_name]["bytes"]
+            will_respond = commands_by_name[command_name]["will_respond"]
+            args = input('arguments= ')
+            await send_command(radio, command_bytes, args, will_respond)
+        elif choice in prompt_options["Help"]:
+            print_help()
+        elif choice in prompt_options["Quit"]:
+            break
 
 
-print_help()
-
-while True:
-    flattend_prompt_options = [v for pov in prompt_options.values() for v in pov]
-    choice = get_input_discrete("Choose an action", flattend_prompt_options)
-    if choice in prompt_options["Receive loop"]:
-        print("Entering receive loop. CTRL-C to exit")
-        while True:
-            try:
-                print(wait_for_message(radio))
-            except KeyboardInterrupt:
-                break
-    elif choice in prompt_options["Upload file"]:
-        path = input('path= ')
-        upload_file(radio, path)
-    elif choice in prompt_options["Send command"]:
-        command_name = get_input_discrete("Select a command", command_map.keys())
-        command_bytes, will_respond = command_map[command_name]
-        args = input('arguments= ')
-        send_command(radio, command_bytes, args, will_respond)
-    elif choice in prompt_options["Help"]:
-        print_help()
-    elif choice in prompt_options["Quit"]:
-        break
+tasko.add_task(gs_shell_main(), 1)
+tasko.run()
