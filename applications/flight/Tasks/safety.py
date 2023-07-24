9d56f09a5fa5f78d5b7944501c23eb45fffa3eaf
tomyyyD <thomas5d@comcast.net>
2023-07-24 16:17:28 -0400

    adding current to safety task

--- applications/flight/Tasks/safety.py
+++ applications/flight/Tasks/safety.py
@@ -9,10 +9,10 @@ class task(Task):
 
     timeout = 60 * 60  # 60 min
 
-    def debug_status(self, vbatt, temp):
-        self.debug(f'Voltage: {vbatt:.2f}V | Temp: {temp:.2f}°C', log=True)
+    def debug_status(self, vbatt, cbatt, temp):
+        self.debug(f'Voltage: {vbatt:.2f}V | Temp: {temp:.2f}°C | Current: {cbatt:.2f}mA', log=True)
 
-    def safe_mode(self, vbatt, temp):
+    def safe_mode(self, vbatt, cbatt, temp):
         # Needs to be done here, and not in transition function due to #306
         cubesat.enable_low_power()
         # margins added to prevent jittering between states
@@ -26,7 +26,7 @@ class task(Task):
                 f'Safe operating conditions reached, switching back to {state_machine.previous_state} mode', log=True)
             state_machine.switch_to(state_machine.previous_state)
 
-    def other_modes(self, vbatt, temp):
+    def other_modes(self, vbatt, cbatt, temp):
         if vbatt < cubesat.LOW_VOLTAGE:
             self.debug(f'Voltage too low ({vbatt:.2f}V < {cubesat.LOW_VOLTAGE:.2f}V) switch to safe mode', log=True)
             state_machine.switch_to('Safe')
@@ -41,9 +41,11 @@ class task(Task):
         If the voltage is too low or the temp is to high, switch to safe mode.
         If the voltage is high enough and the temp is low enough, switch to normal mode.
         """
+        if cubesat.current_sensor:
+            cbatt = cubesat.battery_current
         vbatt = cubesat.battery_voltage
         temp = cubesat.temperature_cpu
         if state_machine.state == 'Safe':
             self.safe_mode(vbatt, temp)
         else:
-            self.other_modes(vbatt, temp)
+            self.other_modes(vbatt, cbatt, temp)
